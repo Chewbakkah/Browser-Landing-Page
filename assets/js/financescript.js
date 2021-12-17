ffaab90d9137acb54fcbcd5e8472334be7b9f1cf
escowin <edwin@escowinart.com>
2021-12-16 19:39:42 -0600

    svg icons 1.0

--- assets/js/financescript.js
+++ assets/js/financescript.js
@@ -1,1 +1,18 @@
 //Finance API widget stuff
+
+var axios = require("axios").default;
+
+var options = {
+  method: 'GET',
+  url: 'https://yfapi.net/v11/finance/quoteSummary/AAPL',
+  params: {modules: 'defaultKeyStatistics,assetProfile'},
+  headers: {
+    'x-api-key': 'RDFVnMFH1btOrenf8xIH8WPlt2dgqCk5i1hNIa68'
+  }
+};
+
+axios.request(options).then(function (response) {
+	console.log(response.data);
+}).catch(function (error) {
+	console.error(error);
+});
